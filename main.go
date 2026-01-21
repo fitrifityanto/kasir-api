175c151288509828a3543fb32b8f77ab7cfb3d5a
fitnr <fitrifityanto@gmail.com>
2026-01-21 08:29:26 +0700

    feat: add /produk endpoint to fetch product list

--- main.go
+++ main.go
@@ -6,7 +6,25 @@ import (
 	"net/http"
 )
 
+type Produk struct {
+	ID    int    `json:"id"`
+	Nama  string `json:"nama"`
+	Harga int    `json:"harga"`
+	Stok  int    `json:"stok"`
+}
+
+var produk = []Produk{
+	{ID: 1, Nama: "Beras", Harga: 70000, Stok: 10},
+	{ID: 2, Nama: "Gula", Harga: 15000, Stok: 20},
+}
+
 func main() {
+	// GET localhost:8080/produk
+	http.HandleFunc("/produk", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(produk)
+	})
+
 	// localhost:8080/health
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
