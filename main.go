1cb93589cf7eee5b4ad464e13a218a64b049e659
fitnr <fitrifityanto@gmail.com>
2026-01-21 08:20:10 +0700

    feat: add /health route

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-
+	// localhost:8080/health
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok", "message": "API running"})
+	})
 	fmt.Println("Server running di localhost:8080")
 
 	err := http.ListenAndServe(":8080", nil)
